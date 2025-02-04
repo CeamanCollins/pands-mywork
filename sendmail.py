9e82e40d0e8958f29dbd9a38b98ab65b28be6858
Ceaman Collins <ceaman.collins@gmail.com>
2025-02-04 18:26:01 +0000

    create file sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,26 @@
+# sendmail.py
+# author: Céaman Collins
+# sends an email via gmail
+
+import smtplib
+from email.mime.text import MIMEText
+
+subject = "Email Subject"
+body = "This is the body of the text message"
+sender = "andrewbeattycourse@gmail.com"
+recipients = ["ceaman.collins@gmail.com", "g00472974@atu.ie"]
+password = "cnkk mvam lcbj sknl"
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+
+send_email(subject, body, sender, recipients, password)
