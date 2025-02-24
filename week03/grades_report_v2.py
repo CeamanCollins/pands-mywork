e0ef3bde8387d259dc08084f233ea7bc334035ed
CeamanCollins <ceaman.collins@gmail.com>
2025-02-24 19:08:01 +0000

    format exploration

---
+++ week03/grades_report_v2.py
@@ -0,0 +1,52 @@
+student = {
+            "name": "John Doe",
+            "subjects": [
+                {
+                    "name": "Mathematics",
+                    "grade": 88,
+                    "comment": "Excellent improvement.",
+                },
+                {
+                    "name": "Science",
+                    "grade": 92,
+                    "comment": "Outstanding performance.",
+                },
+                {
+                    "name": "History",
+                    "grade": 78,
+                    "comment": "Needs to participate more.",
+                },
+                {
+                    "name": "Art",
+                    "grade": 85,
+                    "comment": "Very creative."
+                },
+            ],
+        }
+
+SUBJECT_TEMPLATE = "{name:<15} Grade: {grade:3} Comment: {comment}"
+REPORT_TEMPLATE = """
+Progress Report. Student: {name}
+Average: {average:.2f} / 100
+
+Course Details:
+{subjects_report}
+
+Thank you for reviewing the progress report.
+"""
+
+def build_student_report(student):
+    data = {
+        "name": student["name"],
+        "average": sum(subject["grade"] 
+        for subject in student["subjects"])
+        / len(student["subjects"]),
+        "subjects_report": "\n".join(
+            SUBJECT_TEMPLATE.format(**subject)
+            for subject in student["subjects"]
+        ),
+    }
+
+    return REPORT_TEMPLATE.format(**data)
+
+print(build_student_report(student))
